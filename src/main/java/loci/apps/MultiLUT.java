c66e2c6d875c2ffb24f4ccf39473115cb55a22c6
Curtis Rueden <ctrueden@wisc.edu>
2010-09-14 20:58:53 +0000

    Fix up license for multi-lut project.

--- src/main/java/loci/apps/MultiLUT.java
+++ src/main/java/loci/apps/MultiLUT.java
@@ -3,8 +3,33 @@
 //
 
 /*
-Coded in 2001-2002 by the SSEC Visualization Project.
-Permission is granted to use this code for anything.
+MultiLUT demo application for visually exploring multi-spectral image data.
+
+Copyright (c) 2001, SSEC Visualization Project
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+    * Redistributions of source code must retain the above copyright
+      notice, this list of conditions and the following disclaimer.
+    * Redistributions in binary form must reproduce the above copyright
+      notice, this list of conditions and the following disclaimer in the
+      documentation and/or other materials provided with the distribution.
+    * Neither the name of the SSEC Visualization Project nor the
+      names of its contributors may be used to endorse or promote products
+      derived from this software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDERS OR CONTRIBUTORS BE
+LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+POSSIBILITY OF SUCH DAMAGE.
 */
 
 package loci.apps;
@@ -63,8 +88,10 @@ import visad.util.VisADSlider;
  * An application for weighted spectral mapping.
  *
  * <dl><dt><b>Source code:</b></dt>
- * <dd><a href="https://skyking.microscopy.wisc.edu/trac/java/browser/trunk/components/legacy/multi-lut/src/loci/apps/MultiLUT.java">Trac</a>,
- * <a href="https://skyking.microscopy.wisc.edu/svn/java/trunk/components/legacy/multi-lut/src/loci/apps/MultiLUT.java">SVN</a></dd></dl>
+ * <dd><a href="http://dev.loci.wisc.edu/trac/java/browser/trunk/legacy/multi-lut/src/main/java/loci/apps/MultiLUT.java">Trac</a>,
+ * <a href="http://dev.loci.wisc.edu/svn/java/trunk/legacy/multi-lut/src/main/java/loci/apps/MultiLUT.java">SVN</a></dd></dl>
+ *
+ * @author Curtis Rueden ctrueden at wisc.edu
  */
 public class MultiLUT extends Object implements ActionListener {
 
